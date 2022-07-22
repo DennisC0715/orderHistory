f07fc476dfa8bd6d72236b2ed2253a5227c872fe
Dennis Chen <chensuyuan1989@hotmail.com>
2022-07-22 13:53:36 -0400

    helf to go

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ListItem from "./ActivityList/ListItem";
 import "./ActivityList.css";
-import { userActivitiesData } from "../data";
+
 
 const ActivityList = () => {
   return (
